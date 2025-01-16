836fe5420a893f7b3e5239777a4e0157e481b742
mclee3462 <miniman3462@byui.edu>
2025-01-16 16:01:33 -0700

    modified:   mission.js

--- mission.js
+++ mission.js
@@ -5,10 +5,10 @@ const logo = document.querySelector('.logo img');
 function changeTheme() {
     if (themeSelector.value === 'dark') {
         body.classList.add('dark'); 
-        logo.src = 'images/byui-logo_white.webp'; 
+        logo.src = 'images/byui-logo_white.png'; 
     } else {
         body.classList.remove('dark'); 
-        logo.src = 'images/byui-logo_blue.png'; 
+        logo.src = 'images/byui-logo_blue.webp'; 
     }
 }
 
