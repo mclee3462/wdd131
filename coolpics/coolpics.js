d33d4e0bab74a1e4ba1d59ef8f67dd0545b3f757
mclee3462 <miniman3462@byui.edu>
2025-01-28 16:54:33 -0700

    modified:   coolpics/coolpics.css 	modified:   coolpics/coolpics.js 	modified:   coolpics/index.html

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -7,3 +7,52 @@ document.addEventListener("DOMContentLoaded", () => {
         navMenu.style.display = isVisible ? "none" : "block";
     });
 });
+const menuButton = document.querySelector('.menu-button');
+const menuItems = document.querySelector('.menu-items');
+
+menuButton.addEventListener('click', () => {
+  menuItems.classList.toggle('hide');
+});
+
+function handleResize() {
+  if (window.innerWidth > 1000) {
+    menuItems.classList.remove('hide');
+  } else {
+    menuItems.classList.add('hide');
+  }
+}
+
+window.addEventListener('resize', handleResize);
+handleResize();
+function viewerTemplate(imagePath, altText) {
+    return `
+      <div class="viewer">
+        <button class="close-viewer">X</button>
+        <img src="${imagePath}" alt="${altText}">
+      </div>
+    `;
+  }
+  
+  function viewHandler(event) {
+    const clickedImage = event.target;
+    const imagePath = clickedImage.src.split('-')[0] + '-full.jpeg';
+    const altText = clickedImage.alt;
+  
+    document.body.insertAdjacentHTML('afterbegin', viewerTemplate(imagePath, altText));
+  
+    const closeButton = document.querySelector('.close-viewer');
+    closeButton.addEventListener('click', closeViewer);
+  }
+  
+  function closeViewer() {
+    const viewer = document.querySelector('.viewer');
+    viewer.remove();
+  }
+  
+  const gallery = document.querySelector('.gallery');
+  gallery.addEventListener('click', (event) => {
+    if (event.target.tagName === 'IMG') {
+      viewHandler(event);
+    }
+  });
+  
