9cfaa9654199bba45d0e1d5f334eaa8105df0dda
mclee3462 <miniman3462@byui.edu>
2025-01-28 17:04:49 -0700

    modified:   coolpics/coolpics.css 	modified:   coolpics/coolpics.js 	modified:   coolpics/index.html

--- coolpics/coolpics.js
+++ coolpics/coolpics.js
@@ -1,58 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const menuButton = document.querySelector(".menu-button");
-    const navMenu = document.querySelector("nav ul");
+    const menuButton = document.querySelector('.menu-button');
+    const menuItems = document.querySelector('.menu-items');
 
-    menuButton.addEventListener("click", () => {
-        const isVisible = navMenu.style.display === "block";
-        navMenu.style.display = isVisible ? "none" : "block";
+    menuButton.addEventListener('click', () => {
+        menuItems.classList.toggle('hide');
     });
+
+    function handleResize() {
+        if (window.innerWidth > 1000) {
+            menuItems.classList.remove('hide');
+        } else {
+            menuItems.classList.add('hide');
+        }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); 
 });
-const menuButton = document.querySelector('.menu-button');
-const menuItems = document.querySelector('.menu-items');
 
-menuButton.addEventListener('click', () => {
-  menuItems.classList.toggle('hide');
-});
-
-function handleResize() {
-  if (window.innerWidth > 1000) {
-    menuItems.classList.remove('hide');
-  } else {
-    menuItems.classList.add('hide');
-  }
-}
-
-window.addEventListener('resize', handleResize);
-handleResize();
 function viewerTemplate(imagePath, altText) {
     return `
-      <div class="viewer">
-        <button class="close-viewer">X</button>
-        <img src="${imagePath}" alt="${altText}">
-      </div>
+        <div class="viewer">
+            <button class="close-viewer">X</button>
+            <img src="${imagePath}" alt="${altText}">
+        </div>
     `;
-  }
-  
-  function viewHandler(event) {
+}
+
+function viewHandler(event) {
     const clickedImage = event.target;
     const imagePath = clickedImage.src.split('-')[0] + '-full.jpeg';
     const altText = clickedImage.alt;
-  
+
     document.body.insertAdjacentHTML('afterbegin', viewerTemplate(imagePath, altText));
-  
+
     const closeButton = document.querySelector('.close-viewer');
     closeButton.addEventListener('click', closeViewer);
-  }
-  
-  function closeViewer() {
+}
+
+function closeViewer() {
     const viewer = document.querySelector('.viewer');
     viewer.remove();
-  }
-  
-  const gallery = document.querySelector('.gallery');
-  gallery.addEventListener('click', (event) => {
+}
+
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', (event) => {
     if (event.target.tagName === 'IMG') {
-      viewHandler(event);
+        viewHandler(event);
     }
-  });
-  
+});
+function viewerTemplate(imagePath, altText) {
+    return `
+      <div class="viewer" aria-hidden="true">
+        <button class="close-viewer" aria-label="Close viewer">X</button>
+        <img src="${imagePath}" alt="${altText}">
+      </div>
+    `;
+}
